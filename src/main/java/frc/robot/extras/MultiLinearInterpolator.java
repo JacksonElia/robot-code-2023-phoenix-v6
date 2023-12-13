2926fa0c7e1926aa706d926fc60e7619749123b7
Jack <jackgeorgeelia@gmail.com>
2023-12-13 17:53:58 -0500

    Cleaned up code

--- src/main/java/frc/robot/extras/MultiLinearInterpolator.java
+++ src/main/java/frc/robot/extras/MultiLinearInterpolator.java
@@ -28,11 +28,13 @@ public final class MultiLinearInterpolator {
       return Arrays.copyOfRange(lookupTable[lookupTable.length - 1], 1, lookupTable[0].length);
     }
   
-    for(int i = 0; i < lookupTable.length; i++) {
+    // Goes through each of the y values in the table (e.g. x, y1, y2, y3)
+    for (int i = 0; i < lookupTable.length; i++) {
       if (inputXValue == lookupTable[i][0]) {
         return Arrays.copyOfRange(lookupTable[i], 1, lookupTable[0].length);
       } else if (inputXValue > lookupTable[i][0] && inputXValue < lookupTable[i + 1][0]) {
         double[] interpolatedValues = new double[lookupTable[0].length - 1];
+        // Uses point-slope to get the value
         for(int j = 1; j < lookupTable[0].length; j++) {
           double slope = (lookupTable[i + 1][j] - lookupTable[i][j]) / (lookupTable[i + 1][0] - lookupTable[i][0]);
           double yIntercept = lookupTable[i][j];
